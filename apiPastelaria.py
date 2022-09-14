f1f5caab03d8a7da9a1bf1b1003f955e3da08fdd
Abc Bolinhas <abc@bolinhas.com>
2022-09-14 20:10:18 -0300

    entregaAula140922

--- apiPastelaria.py
+++ apiPastelaria.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-import FuncionarioDAO
-import ClienteDAO
-import ProdutoDAO
+
+import db
+# import das classes de modelo de persistência
+from mod_funcionario.FuncionarioModel import FuncionarioDB
+
+# import das classes com as rotas/endpoints
+from mod_funcionario import FuncionarioDAO
 
 app = FastAPI()
 
+# mapeamento das rotas/endpoints
 app.include_router(FuncionarioDAO.router)
-app.include_router(ProdutoDAO.router)
-app.include_router(ClienteDAO.router)
+
+# cria, caso não existam, as tabelas de todos os modelos importados
+
+app.include_router(FuncionarioDAO.router)
+
+db.criaTabelas()
